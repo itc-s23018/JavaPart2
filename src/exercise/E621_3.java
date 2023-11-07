f9da7fb28100a3016332a164119a210afe2ed00f
itc-s23018 <s23018@std.it-college.ac.jp>
2023-11-07 16:57:12 +0900

    chapter6-2 練習6-2-1 終了

--- src/exercise/E621_3.java
+++ src/exercise/E621_3.java
@@ -3,6 +3,6 @@ package exercise;
 public class E621_3 {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(5, 3);
-        System.out.println("面積=%.2f%n" + rectangle.area());
+        System.out.printf("面積=%.2f%n", rectangle.area());
     }
 }
