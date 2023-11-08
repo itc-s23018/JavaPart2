9bfc8feefa444e0d7f33170bdcf0eaefa8c2005c
itc-s23018 <s23018@std.it-college.ac.jp>
2023-11-08 16:38:48 +0900

    chapter6-3 練習6-3-1 終了

---
+++ src/chapter6/Sample2.java
@@ -0,0 +1,24 @@
+package chapter6;
+
+public class Sample2 {
+    public static void main(String[] args) {
+
+        Tax tax = new Tax(100, "田中宏", 300000, 100000);
+        tax.setKoujo(120000);
+
+        System.out.println("控除額=" + tax.getKoujo());
+
+        //追加で検証
+        System.out.println("所得を上回る控除額を設定");
+        tax.setKoujo(400000);
+        System.out.println("控除額=" + tax.getKoujo());
+        System.out.println("所得税=" + tax.zeigaku());
+        //追加２
+        System.out.println("所得を下回る控除額を設定");
+        tax.setKoujo(250000);
+        System.out.println("控除額=" + tax.getKoujo());
+        System.out.println("所得税=" + tax.zeigaku());
+
+
+    }
+}
--- src/chapter6/Tax.java
+++ src/chapter6/Tax.java
@@ -1,16 +1,83 @@
 package chapter6;
 
+import java.util.Objects;
+
 public class Tax {
     private int number;
     private String name;
     private int shotoku;
     private int koujo;
 
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getShotoku() {
+        return shotoku;
+    }
+
+    public void setShotoku(int shotoku) {
+        this.shotoku = shotoku;
+        if (koujo > this.shotoku){
+            //設定済みの控除額より所得税が下回ったら調整する。
+            koujo = this.shotoku;
+        }
+    }
+
+    public int getKoujo() {
+        return koujo;
+    }
+
+    public void setKoujo(int koujo) {
+        if(shotoku < koujo){
+            // koujoがshotokuを上回ってしまったら
+            //this.koujoはshotokuと同じ値で設定する
+            this.koujo = shotoku;
+        }else {
+            this.koujo = koujo;
+        }
+    }
+
     public Tax(int number, String name, int shotoku, int koujo) {
         this.number = number;
         this.name = name;
         this.shotoku = shotoku;
         this.koujo = koujo;
+    }
+
+    @Override
+    public String toString() {
+        return "Tax{" +
+                "number=" + number +
+                ", name='" + name + '\'' +
+                ", shotoku=" + shotoku +
+                ", koujo=" + koujo +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tax tax = (Tax) o;
+        return number == tax.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 
     public int zeigaku(){
